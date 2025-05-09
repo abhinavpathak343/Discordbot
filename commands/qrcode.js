cfc236a7b3ca7bd62fa4594977c9b87bdd7f9a7e
abhinavpathak343 <abhinavpathak343@gmail.com>
2025-05-09 23:17:44 +0530

    hi from prod

--- commands/qrcode.js
+++ commands/qrcode.js
@@ -1,6 +1,7 @@
 import {
     SlashCommandBuilder
 } from 'discord.js';
+import QRCode from 'qrcode';
 
 export const data = new SlashCommandBuilder()
     .setName('qrcode')
@@ -12,7 +13,32 @@ export const data = new SlashCommandBuilder()
     );
 
 export async function execute(interaction) {
-    const url = interaction.options.getString('url');
-    // Your QR code generation logic here
-    await interaction.reply('QR code feature coming soon!');
+    try {
+        const url = interaction.options.getString('url');
+
+        // Generate QR code as data URL
+        const qrCodeDataUrl = await QRCode.toDataURL(url, {
+            errorCorrectionLevel: 'H',
+            margin: 1,
+            width: 400
+        });
+
+        // Convert data URL to buffer
+        const qrCodeBuffer = Buffer.from(qrCodeDataUrl.split(',')[1], 'base64');
+
+        // Send the QR code as an image
+        await interaction.reply({
+            content: `Here's the QR code for ${url}:`,
+            files: [{
+                attachment: qrCodeBuffer,
+                name: 'qrcode.png'
+            }]
+        });
+    } catch (error) {
+        console.error('Error generating QR code:', error);
+        await interaction.reply({
+            content: 'Sorry, there was an error generating the QR code. Please make sure the URL is valid.',
+            ephemeral: true
+        });
+    }
 }
